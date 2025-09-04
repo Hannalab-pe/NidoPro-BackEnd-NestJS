02e747998e8ce0f1163ab75dfcdeddd59eaa028d
EduardoHuarcaya-DEV <waltereduardohm@gmail.com>
2025-09-04 16:29:14 -0500

    Implementacion imagenUrl del trabajador

--- src/trabajador/dto/create-trabajador.dto.ts
+++ src/trabajador/dto/create-trabajador.dto.ts
@@ -1,51 +1,64 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { IsString, IsOptional, IsBoolean, IsUUID, IsEnum, IsEmail } from "class-validator";
-import { UserRole } from "../../enums/roles.enum";
-import { TipoDocumento } from "../../enums/tipo-documento.enum";
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsUUID,
+  IsEnum,
+  IsEmail,
+} from 'class-validator';
+import { TipoDocumento } from '../../enums/tipo-documento.enum';
 
 export class CreateTrabajadorDto {
-    @ApiProperty()
-    @IsString()
-    nombre: string;
+  @ApiProperty()
+  @IsString()
+  nombre: string;
 
-    @ApiProperty()
-    @IsString()
-    apellido: string;
+  @ApiProperty()
+  @IsString()
+  apellido: string;
 
-    @ApiProperty({
-        enum: TipoDocumento,
-        description: 'Tipo de documento'
-    })
-    @IsEnum(TipoDocumento, { message: 'Tipo de documento debe ser uno de los valores válidos' })
-    tipoDocumento: TipoDocumento;
+  @ApiProperty({
+    enum: TipoDocumento,
+    description: 'Tipo de documento',
+  })
+  @IsEnum(TipoDocumento, {
+    message: 'Tipo de documento debe ser uno de los valores válidos',
+  })
+  tipoDocumento: TipoDocumento;
 
-    @ApiProperty()
-    @IsString()
-    nroDocumento: string;
+  @ApiProperty()
+  @IsString()
+  nroDocumento: string;
 
-    @ApiProperty({ required: false })
-    @IsString()
-    @IsOptional()
-    direccion?: string;
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  direccion?: string;
 
-    @ApiProperty({ required: false })
-    @IsEmail({}, { message: 'Debe ser un email válido' })
-    @IsOptional()
-    correo?: string;
+  @ApiProperty({ required: false })
+  @IsEmail({}, { message: 'Debe ser un email válido' })
+  @IsOptional()
+  correo?: string;
 
-    @ApiProperty({ required: false })
-    @IsString()
-    @IsOptional()
-    telefono?: string;
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  telefono?: string;
 
-    @ApiProperty({ default: true, required: false })
-    @IsBoolean()
-    @IsOptional()
-    estaActivo: boolean;
+  @ApiProperty({ default: true, required: false })
+  @IsBoolean()
+  @IsOptional()
+  estaActivo: boolean;
 
-    @ApiProperty({
-        description: 'ID del rol a asignar al trabajador'
-    })
-    @IsUUID(4, { message: 'ID de rol debe ser un UUID válido' })
-    idRol: string;
+  @ApiProperty({ required: false })
+  @IsString()
+  @IsOptional()
+  imagenUrl?: string;
+
+  @ApiProperty({
+    description: 'ID del rol a asignar al trabajador',
+  })
+  @IsUUID(4, { message: 'ID de rol debe ser un UUID válido' })
+  idRol: string;
 }
--- src/trabajador/entities/trabajador.entity.ts
+++ src/trabajador/entities/trabajador.entity.ts
@@ -1,239 +1,242 @@
 import {
-    Column,
-    Entity,
-    Index,
-    JoinColumn,
-    ManyToOne,
-    OneToMany,
-} from "typeorm";
-import { Usuario } from "src/usuario/entities/usuario.entity";
-import { AsignacionAula } from "src/asignacion-aula/entities/asignacion-aula.entity";
-import { AsignacionCurso } from "src/asignacion-curso/entities/asignacion-curso.entity";
-import { ContratoTrabajador } from "src/contrato-trabajador/entities/contrato-trabajador.entity";
-import { Cronograma } from "src/cronograma/entities/cronograma.entity";
-import { DetallePlanilla } from "src/detalle-planilla/entities/detalle-planilla.entity";
-import { EvaluacionDocenteBimestral } from "src/evualuacion-docente-bimestral/entities/evualuacion-docente-bimestral.entity";
-import { HistorialContrato } from "src/historial-contrato/entities/historial-contrato.entity";
-import { Informe } from "src/informe/entities/informe.entity";
-import { ObservacionDocente } from "src/observacion-docente/entities/observacion-docente.entity";
-import { PensionEstudiante } from "src/pension-estudiante/entities/pension-estudiante.entity";
-import { PlanillaMensual } from "src/planilla-mensual/entities/planilla-mensual.entity";
-import { ProgramacionMensual } from "src/programacion-mensual/entities/programacion-mensual.entity";
-import { RenovacionContrato } from "src/renovacion-contrato/entities/renovacion-contrato.entity";
-import { Rol } from "src/rol/entities/rol.entity";
-import { SeguroTrabajador } from "src/seguro-trabajador/entities/seguro-trabajador.entity";
-import { SueldoTrabajador } from "src/sueldo-trabajador/entities/sueldo-trabajador.entity";
-import { Tarea } from "src/tarea/entities/tarea.entity";
+  Column,
+  Entity,
+  Index,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import { Usuario } from 'src/usuario/entities/usuario.entity';
+import { AsignacionAula } from 'src/asignacion-aula/entities/asignacion-aula.entity';
+import { AsignacionCurso } from 'src/asignacion-curso/entities/asignacion-curso.entity';
+import { ContratoTrabajador } from 'src/contrato-trabajador/entities/contrato-trabajador.entity';
+import { Cronograma } from 'src/cronograma/entities/cronograma.entity';
+import { DetallePlanilla } from 'src/detalle-planilla/entities/detalle-planilla.entity';
+import { EvaluacionDocenteBimestral } from 'src/evualuacion-docente-bimestral/entities/evualuacion-docente-bimestral.entity';
+import { HistorialContrato } from 'src/historial-contrato/entities/historial-contrato.entity';
+import { Informe } from 'src/informe/entities/informe.entity';
+import { ObservacionDocente } from 'src/observacion-docente/entities/observacion-docente.entity';
+import { PensionEstudiante } from 'src/pension-estudiante/entities/pension-estudiante.entity';
+import { PlanillaMensual } from 'src/planilla-mensual/entities/planilla-mensual.entity';
+import { ProgramacionMensual } from 'src/programacion-mensual/entities/programacion-mensual.entity';
+import { RenovacionContrato } from 'src/renovacion-contrato/entities/renovacion-contrato.entity';
+import { Rol } from 'src/rol/entities/rol.entity';
+import { SeguroTrabajador } from 'src/seguro-trabajador/entities/seguro-trabajador.entity';
+import { SueldoTrabajador } from 'src/sueldo-trabajador/entities/sueldo-trabajador.entity';
+import { Tarea } from 'src/tarea/entities/tarea.entity';
 
 @Index(
-    "idx_trabajador_info",
-    ["apellido", "estaActivo", "nombre", "nroDocumento", "tipoDocumento"],
-    {}
+  'idx_trabajador_info',
+  ['apellido', 'estaActivo', 'nombre', 'nroDocumento', 'tipoDocumento'],
+  {},
 )
-@Index("idx_trabajador_contacto", ["correo", "telefono"], {})
-@Index("trabajador_correo_key", ["correo"], { unique: true })
-@Index("trabajador_pkey", ["idTrabajador"], { unique: true })
-@Index("trabajador_nro_documento_key", ["nroDocumento"], { unique: true })
-@Index("trabajador_telefono_key", ["telefono"], { unique: true })
-@Entity("trabajador", { schema: "public" })
+@Index('idx_trabajador_contacto', ['correo', 'telefono'], {})
+@Index('trabajador_correo_key', ['correo'], { unique: true })
+@Index('trabajador_pkey', ['idTrabajador'], { unique: true })
+@Index('trabajador_nro_documento_key', ['nroDocumento'], { unique: true })
+@Index('trabajador_telefono_key', ['telefono'], { unique: true })
+@Entity('trabajador', { schema: 'public' })
 export class Trabajador {
-    @Column("uuid", {
-        primary: true,
-        name: "id_trabajador",
-        default: () => "uuid_generate_v4()",
-    })
-    idTrabajador: string;
+  @Column('uuid', {
+    primary: true,
+    name: 'id_trabajador',
+    default: () => 'uuid_generate_v4()',
+  })
+  idTrabajador: string;
 
-    @Column("character varying", { name: "nombre", length: 100 })
-    nombre: string;
+  @Column('character varying', { name: 'nombre', length: 100 })
+  nombre: string;
 
-    @Column("character varying", { name: "apellido", length: 100 })
-    apellido: string;
+  @Column('character varying', { name: 'apellido', length: 100 })
+  apellido: string;
 
-    @Column("character varying", { name: "tipo_documento", length: 20 })
-    tipoDocumento: string;
+  @Column('character varying', { name: 'tipo_documento', length: 20 })
+  tipoDocumento: string;
 
-    @Column("character varying", {
-        name: "nro_documento",
-        unique: true,
-        length: 20,
-    })
-    nroDocumento: string;
+  @Column('character varying', {
+    name: 'nro_documento',
+    unique: true,
+    length: 20,
+  })
+  nroDocumento: string;
 
-    @Column("text", { name: "direccion", nullable: true })
-    direccion: string | null;
+  @Column('text', { name: 'direccion', nullable: true })
+  direccion: string | null;
 
-    @Column("character varying", {
-        name: "correo",
-        nullable: true,
-        unique: true,
-        length: 255,
-    })
-    correo: string | null;
+  @Column('character varying', {
+    name: 'correo',
+    nullable: true,
+    unique: true,
+    length: 255,
+  })
+  correo: string | null;
 
-    @Column("character varying", {
-        name: "telefono",
-        nullable: true,
-        unique: true,
-        length: 20,
-    })
-    telefono: string | null;
+  @Column('character varying', {
+    name: 'telefono',
+    nullable: true,
+    unique: true,
+    length: 20,
+  })
+  telefono: string | null;
 
-    @Column("boolean", {
-        name: "esta_activo",
-        nullable: true,
-        default: () => "true",
-    })
-    estaActivo: boolean | null;
+  @Column('boolean', {
+    name: 'esta_activo',
+    nullable: true,
+    default: () => 'true',
+  })
+  estaActivo: boolean | null;
 
-    @OneToMany(
-        () => AsignacionAula,
-        (asignacionAula) => asignacionAula.idTrabajador
-    )
-    asignacionAulas: AsignacionAula[];
+  @Column('text', { name: 'imagen_url', nullable: true })
+  imagenUrl: string | null;
 
-    @OneToMany(
-        () => AsignacionCurso,
-        (asignacionCurso) => asignacionCurso.idTrabajador
-    )
-    asignacionCursos: AsignacionCurso[];
+  @OneToMany(
+    () => AsignacionAula,
+    (asignacionAula) => asignacionAula.idTrabajador,
+  )
+  asignacionAulas: AsignacionAula[];
 
-    @OneToMany(
-        () => ContratoTrabajador,
-        (contratoTrabajador) => contratoTrabajador.aprobadoPor
-    )
-    contratoTrabajadors: ContratoTrabajador[];
+  @OneToMany(
+    () => AsignacionCurso,
+    (asignacionCurso) => asignacionCurso.idTrabajador,
+  )
+  asignacionCursos: AsignacionCurso[];
 
-    @OneToMany(
-        () => ContratoTrabajador,
-        (contratoTrabajador) => contratoTrabajador.creadoPor
-    )
-    contratoTrabajadors2: ContratoTrabajador[];
+  @OneToMany(
+    () => ContratoTrabajador,
+    (contratoTrabajador) => contratoTrabajador.aprobadoPor,
+  )
+  contratoTrabajadors: ContratoTrabajador[];
 
-    @OneToMany(
-        () => ContratoTrabajador,
-        (contratoTrabajador) => contratoTrabajador.idTrabajador2
-    )
-    contratoTrabajadors3: ContratoTrabajador[];
+  @OneToMany(
+    () => ContratoTrabajador,
+    (contratoTrabajador) => contratoTrabajador.creadoPor,
+  )
+  contratoTrabajadors2: ContratoTrabajador[];
 
-    @OneToMany(() => Cronograma, (cronograma) => cronograma.idTrabajador)
-    cronogramas: Cronograma[];
+  @OneToMany(
+    () => ContratoTrabajador,
+    (contratoTrabajador) => contratoTrabajador.idTrabajador2,
+  )
+  contratoTrabajadors3: ContratoTrabajador[];
 
-    @OneToMany(
-        () => DetallePlanilla,
-        (detallePlanilla) => detallePlanilla.idTrabajador2
-    )
-    detallePlanillas: DetallePlanilla[];
+  @OneToMany(() => Cronograma, (cronograma) => cronograma.idTrabajador)
+  cronogramas: Cronograma[];
 
-    @OneToMany(
-        () => EvaluacionDocenteBimestral,
-        (evaluacionDocenteBimestral) => evaluacionDocenteBimestral.idCoordinador
-    )
-    evaluacionDocenteBimestrals: EvaluacionDocenteBimestral[];
+  @OneToMany(
+    () => DetallePlanilla,
+    (detallePlanilla) => detallePlanilla.idTrabajador2,
+  )
+  detallePlanillas: DetallePlanilla[];
 
-    @OneToMany(
-        () => EvaluacionDocenteBimestral,
-        (evaluacionDocenteBimestral) => evaluacionDocenteBimestral.idTrabajador2
-    )
-    evaluacionDocenteBimestrals2: EvaluacionDocenteBimestral[];
+  @OneToMany(
+    () => EvaluacionDocenteBimestral,
+    (evaluacionDocenteBimestral) => evaluacionDocenteBimestral.idCoordinador,
+  )
+  evaluacionDocenteBimestrals: EvaluacionDocenteBimestral[];
 
-    @OneToMany(
-        () => HistorialContrato,
-        (historialContrato) => historialContrato.idTrabajador2
-    )
-    historialContratoes: HistorialContrato[];
+  @OneToMany(
+    () => EvaluacionDocenteBimestral,
+    (evaluacionDocenteBimestral) => evaluacionDocenteBimestral.idTrabajador2,
+  )
+  evaluacionDocenteBimestrals2: EvaluacionDocenteBimestral[];
 
-    @OneToMany(
-        () => HistorialContrato,
-        (historialContrato) => historialContrato.realizadoPor
-    )
-    historialContratoes2: HistorialContrato[];
+  @OneToMany(
+    () => HistorialContrato,
+    (historialContrato) => historialContrato.idTrabajador2,
+  )
+  historialContratoes: HistorialContrato[];
 
-    @OneToMany(() => Informe, (informe) => informe.idTrabajador)
-    informes: Informe[];
+  @OneToMany(
+    () => HistorialContrato,
+    (historialContrato) => historialContrato.realizadoPor,
+  )
+  historialContratoes2: HistorialContrato[];
 
-    @OneToMany(
-        () => ObservacionDocente,
-        (observacionDocente) => observacionDocente.idCoordinador
-    )
-    observacionDocentes: ObservacionDocente[];
+  @OneToMany(() => Informe, (informe) => informe.idTrabajador)
+  informes: Informe[];
 
-    @OneToMany(
-        () => ObservacionDocente,
-        (observacionDocente) => observacionDocente.idTrabajador
-    )
-    observacionDocentes2: ObservacionDocente[];
+  @OneToMany(
+    () => ObservacionDocente,
+    (observacionDocente) => observacionDocente.idCoordinador,
+  )
+  observacionDocentes: ObservacionDocente[];
 
-    @OneToMany(
-        () => PensionEstudiante,
-        (pensionEstudiante) => pensionEstudiante.registradoPor
-    )
-    pensionEstudiantes: PensionEstudiante[];
+  @OneToMany(
+    () => ObservacionDocente,
+    (observacionDocente) => observacionDocente.idTrabajador,
+  )
+  observacionDocentes2: ObservacionDocente[];
 
-    @OneToMany(
-        () => PlanillaMensual,
-        (planillaMensual) => planillaMensual.aprobadoPor
-    )
-    planillaMensuals: PlanillaMensual[];
+  @OneToMany(
+    () => PensionEstudiante,
+    (pensionEstudiante) => pensionEstudiante.registradoPor,
+  )
+  pensionEstudiantes: PensionEstudiante[];
 
-    @OneToMany(
-        () => PlanillaMensual,
-        (planillaMensual) => planillaMensual.generadoPor
-    )
-    planillaMensuals2: PlanillaMensual[];
+  @OneToMany(
+    () => PlanillaMensual,
+    (planillaMensual) => planillaMensual.aprobadoPor,
+  )
+  planillaMensuals: PlanillaMensual[];
 
-    @OneToMany(
-        () => PlanillaMensual,
-        (planillaMensual) => planillaMensual.pagadoPor
-    )
-    planillaMensuals3: PlanillaMensual[];
+  @OneToMany(
+    () => PlanillaMensual,
+    (planillaMensual) => planillaMensual.generadoPor,
+  )
+  planillaMensuals2: PlanillaMensual[];
 
-    @OneToMany(
-        () => ProgramacionMensual,
-        (programacionMensual) => programacionMensual.trabajador
-    )
-    programacionMensuals: ProgramacionMensual[];
+  @OneToMany(
+    () => PlanillaMensual,
+    (planillaMensual) => planillaMensual.pagadoPor,
+  )
+  planillaMensuals3: PlanillaMensual[];
 
-    @OneToMany(
-        () => RenovacionContrato,
-        (renovacionContrato) => renovacionContrato.aprobadoPor
-    )
-    renovacionContratoes: RenovacionContrato[];
+  @OneToMany(
+    () => ProgramacionMensual,
+    (programacionMensual) => programacionMensual.trabajador,
+  )
+  programacionMensuals: ProgramacionMensual[];
 
-    @OneToMany(
-        () => SeguroTrabajador,
-        (seguroTrabajador) => seguroTrabajador.creadoPor
-    )
-    seguroTrabajadors: SeguroTrabajador[];
+  @OneToMany(
+    () => RenovacionContrato,
+    (renovacionContrato) => renovacionContrato.aprobadoPor,
+  )
+  renovacionContratoes: RenovacionContrato[];
 
-    @OneToMany(
-        () => SeguroTrabajador,
-        (seguroTrabajador) => seguroTrabajador.idTrabajador2
-    )
-    seguroTrabajadors2: SeguroTrabajador[];
+  @OneToMany(
+    () => SeguroTrabajador,
+    (seguroTrabajador) => seguroTrabajador.creadoPor,
+  )
+  seguroTrabajadors: SeguroTrabajador[];
 
-    @OneToMany(
-        () => SueldoTrabajador,
-        (sueldoTrabajador) => sueldoTrabajador.creadoPor
-    )
-    sueldoTrabajadors: SueldoTrabajador[];
+  @OneToMany(
+    () => SeguroTrabajador,
+    (seguroTrabajador) => seguroTrabajador.idTrabajador2,
+  )
+  seguroTrabajadors2: SeguroTrabajador[];
 
-    @OneToMany(
-        () => SueldoTrabajador,
-        (sueldoTrabajador) => sueldoTrabajador.idTrabajador
-    )
-    sueldoTrabajadors2: SueldoTrabajador[];
+  @OneToMany(
+    () => SueldoTrabajador,
+    (sueldoTrabajador) => sueldoTrabajador.creadoPor,
+  )
+  sueldoTrabajadors: SueldoTrabajador[];
 
-    @OneToMany(() => Tarea, (tarea) => tarea.idTrabajador)
-    tareas: Tarea[];
+  @OneToMany(
+    () => SueldoTrabajador,
+    (sueldoTrabajador) => sueldoTrabajador.idTrabajador,
+  )
+  sueldoTrabajadors2: SueldoTrabajador[];
 
-    @ManyToOne(() => Rol, (rol) => rol.trabajadores, { onDelete: "RESTRICT" })
-    @JoinColumn([{ name: "id_rol", referencedColumnName: "idRol" }])
-    idRol: Rol;
+  @OneToMany(() => Tarea, (tarea) => tarea.idTrabajador)
+  tareas: Tarea[];
 
-    @ManyToOne(() => Usuario, (usuario) => usuario.trabajadores, {
-        onDelete: "CASCADE",
-    })
-    @JoinColumn([{ name: "id_usuario", referencedColumnName: "idUsuario" }])
-    idUsuario: Usuario;
+  @ManyToOne(() => Rol, (rol) => rol.trabajadores, { onDelete: 'RESTRICT' })
+  @JoinColumn([{ name: 'id_rol', referencedColumnName: 'idRol' }])
+  idRol: Rol;
+
+  @ManyToOne(() => Usuario, (usuario) => usuario.trabajadores, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn([{ name: 'id_usuario', referencedColumnName: 'idUsuario' }])
+  idUsuario: Usuario;
 }
